01ce98e0a90f43a3a42e699d9bd794d6f907c910
Kattia <kattbcc17@byui.edu>
2025-01-25 19:28:42 -0500

    created contact-item

---
+++ src/app/contacts/contact-item/contact-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-contact-item',
+  imports: [],
+  templateUrl: './contact-item.component.html',
+  styleUrl: './contact-item.component.css'
+})
+export class ContactItemComponent {
+
+}
