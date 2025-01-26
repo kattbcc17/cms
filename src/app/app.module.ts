3846b79cd61b1136d7fd55a1b940cac4c3c3a0a0
Kattia <kattbcc17@byui.edu>
2025-01-25 22:08:40 -0500

    documents component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';  
+
+// Import components
 import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
-import { RouterModule } from '@angular/router';  
+import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([]),
+  declarations: [
     HeaderComponent,  
     ContactsComponent,
     ContactListComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    ContactItemComponent 
   ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([]),  // Import Angular modules
+  ],
+  exports: [ContactListComponent],  // Export any module/component if needed
   providers: [],
+  bootstrap: [ContactsComponent]  // Bootstrapping your main component (adjust if necessary)
 })
 export class AppModule {}
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,36 +1,40 @@
 import { Component } from '@angular/core';
 import { Contact } from '../contact.model';
 import { CommonModule } from '@angular/common';
+import { ContactItemComponent } from '../contact-item/contact-item.component';  // Import ContactItemComponent
+
 
 @Component({
   standalone: true,
+  imports: [CommonModule, ContactItemComponent],
   selector: 'cms-contact-list',
-  imports: [CommonModule],
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
   
 export class ContactListComponent {
-  get imagePath() {
-    return 'assets/images/';
-  }
   
   contacts: Contact[] = [
     new Contact(
-      "1",
-      "R. Kent Jackson",
-      "jacksonk@byui.edu",
-      "208-496-3771",
-      "jacksonk.jpg",
-      null
+      '1', 
+      'R. Kent Jackson', 
+      'jacksonk@byui.edu', 
+      '208-496-3771',
+      '../../assets/images/jacksonk.jpg',
+      null,
     ),
     new Contact(
-      "2",
-      "Rex Barzee",
-      "barzeer@byui.edu",
-      "208-496-3768",
-      "barzeer.jpg",
-      null
-    )
+      '2', 
+      'Rex Barzee', 
+      'barzeer@byui.edu',
+      '208-496-3768',
+      '../../assets/images/barzeer.jpg',
+      null,
+    ),
   ];
+
+
+  onNewContact() {
+    console.log('clicked!');
+  }
 }
