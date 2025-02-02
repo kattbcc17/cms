dd529df4da887eb4aa531f4e8aba73a7c4f09f6e
Kattia <kattbcc17@byui.edu>
2025-02-01 22:27:41 -0500

    recreated project

--- src/app/documents/document.model.ts
+++
@@ -1,9 +0,0 @@
-export class Document {
-    constructor(
-      public id: string,
-      public name: string,
-      public description: string,
-      public imageUrl: string,
-      public children: []
-    ) {}
-  }
