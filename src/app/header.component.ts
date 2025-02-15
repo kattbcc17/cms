eedbff2214d3db543aa8a162c87bddab5de85539
Kattia <kattbcc17@byui.edu>
2025-02-15 10:55:49 -0500

    delete the imports for the EventEmitter and Output classes

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,14 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cms-header',
   standalone: false,
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  @Output() selectedFeatureEvent = new EventEmitter<string>();
-
-  onSelected(selectedEvent: string) {
-    this.selectedFeatureEvent.emit(selectedEvent);
-  }
-}
+export class HeaderComponent {}
