44d3d20c3c89e64e1b5462e2e4859820fe74c2e0
Kattia <kattbcc17@byui.edu>
2025-03-08 14:11:58 -0500

    Modify the MessageService to Perform HTTP Get/Put Requests

--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -1,4 +1,7 @@
 import { EventEmitter, Injectable, Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
 
@@ -6,15 +9,20 @@ import { MOCKMESSAGES } from './MOCKMESSAGES';
   providedIn: 'root',
 })
 export class MessageService {
-  messages: Message[];
+  messages: Message[] = [];
+  maxMessageId: number;
   @Output() messageChangedEvent = new EventEmitter<Message[]>();
+  messageListChangedEvent = new Subject<Message[]>();
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.messages = MOCKMESSAGES;
+    this.maxMessageId = this.getMaxId();
   }
 
-  getMessages(): Message[] {
-    return this.messages.slice();
+  getMessages() {
+    return this.http.get<Message[]>(
+      'https://cmspro-514e6-default-rtdb.firebaseio.com/messages.json'
+    );
   }
 
   getMessageId(id: string): Message {
@@ -26,8 +34,57 @@ export class MessageService {
     return null;
   }
 
+  fetchMessages() {
+    this.getMessages().subscribe(
+      (messages: Message[]) => {
+        this.messages = messages;
+        this.maxMessageId = this.getMaxId();
+        this.messages.sort((curr, next) => {
+          if (curr.id < next.id) {
+            return -1;
+          } else if (curr.id > next.id) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        this.messageListChangedEvent.next(this.messages.slice());
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  storeMessages() {
+    let originalMessages = JSON.stringify(this.messages);
+    const headers = new HttpHeaders({ 'content-type': 'application/json' });
+
+    this.http
+      .put(
+        'https://cmspro-514e6-default-rtdb.firebaseio.com/messages.json',
+        originalMessages,
+        { headers }
+      )
+      .subscribe(() =>
+        this.messageListChangedEvent.next(this.messages.slice())
+      );
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    this.messages.forEach((message) => {
+      let currentId = +message.id;
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    });
+    return maxId;
+  }
+
   addMessage(message: Message) {
     this.messages.push(message);
     this.messageChangedEvent.emit(this.messages.slice());
+    this.storeMessages();
   }
 }
